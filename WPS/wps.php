3d6bab90e994360a872d9dd2cbb78b0fcaef4fbe
Ali <47595754+maestro6321@users.noreply.github.com>
2023-02-20 11:47:33 +0330

    Add files via upload

---
+++ WPS/wps.php
@@ -0,0 +1,50 @@
+<?php
+/*
+Plugin Name: wprdpress visitors users
+Plugin URI: http://wordpress.org/plugins/hello-dolly/
+Description: save visitors information.
+Author: Ali
+Version: 1.0.0
+Author URI: http://tt.tt/
+*/
+defined('ABSPATH') || exit() ;
+//define constracts for wps
+define('wps_dir',trailingslashit( plugin_dir_path(__FILE__) ));
+// define('wps_url',trailingslashit( plugin_dir_url(__FILE__) ));
+define('wps_url',get_template_directory_uri().'/wps'.'/');
+define('wps_inc',trailingslashit( wps_dir.'inc' ));
+define('wps_tpl',trailingslashit( wps_dir.'tpl' ));
+define('wps_css',trailingslashit( wps_url.'assets'.'/'.'css' ));
+define('wps_js',trailingslashit( wps_url.'assets'.'/'.'js' ));
+define('wps_images',trailingslashit( wps_url.'assets'.'/'.'images' ));
+define('wps_fonts',trailingslashit( wps_url.'assets'.'/'.'fonts' ));
+
+// write activation && deactivation hook callback
+add_action( 'wps_load_style_admin', 'loading_style_script_admin' );
+function loading_style_script_admin() {
+    
+	wp_enqueue_style( 'bootstrap', wps_css . 'bootstrap.css');
+	// wp_enqueue_script( 'jquery_admin', wps_js . 'jquery-3.6.3.min.js', array(), '1.0.0', true );
+	// wp_enqueue_script( 'popper_admin', wps_js . 'popper.min.js', array(), '1.0.0', true );
+	wp_enqueue_script( 'bootstrap_admin', wps_js . 'bootstrap.bundle.js', array(), '1.0.0', true );
+    // define('CONCATENATE_SCRIPTS', false);
+}
+
+function wps_activation(){}
+function wps_deactivation(){}
+register_activation_hook( __FILE__, 'wps_activation' );
+register_deactivation_hook( __FILE__, 'wps_deactivation' );
+
+//
+if(is_admin()){
+    include wps_inc."backend.php";
+    
+    do_action( 'wps_load_style_admin');
+    
+
+}else{
+    include wps_inc."frontend.php";
+    // include wps_inc."backend.php";
+}
+
+?>
---
+++ inc/index-header.php
@@ -0,0 +1,13 @@
+<section class="header-section">
+      <div class="center-dive">
+        <h1 class="display-4"><?php echo get_pk_options('company_name'); ?></h1>
+        <h4><?php echo get_pk_options('company_tittle'); ?></h4>
+        <svg width="0" height="0">
+          <defs>
+            <clippath id="myClip" clipPathUnits="objectBoundingBox">
+              <path d="M0,0 1,0 1,0.9 C 1,0.9, 0.77,1, 0.5,1 0.23,1, 0,0.9,0,0.9z" />
+            </clippath>
+          </defs>
+        </svg>
+      </div>
+    </section>
