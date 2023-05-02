1e1f7678eb0488eb8383259c1abe141a7d9d453d
maestro6321 <grand.master@live.com>
2023-05-02 15:51:33 +0330

    fix bug on handel project list output

--- WPS/inc/frontend.php
+++ WPS/inc/frontend.php
@@ -16,28 +16,63 @@ function get_pk_projects($id){
 	$counter=0;
 	if ($id=='1') {
 		$rows = $wpdb->get_results( "SELECT * FROM `wp_pk_projects` WHERE `status`='تکمیل'	");
+		foreach ( $rows as $row )
+		{
+			$counter+=1;
+			$message .='<tr>';
+			$message .= '<th scope="row">'. $counter .'</th>';
+			$message .= '<th>'.$row->employer.'</th>';
+			$message .= '<th>'.$row->place.'</th>';
+			$message .= '<th>'.$row->contract.'</th>';
+			$message .= '<th>'.$row->status.'</th>';
+			$message .= '<th>'.$row->volume.'</th>';
+			$message .= '<th>'.$row->date.'</th>';
+	
+			$message .='</tr>';
+		}
+		 
+		// Output needs to be return
+		return $message;
 	}elseif ($id==2){
 		$rows = $wpdb->get_results( "SELECT * FROM `wp_pk_projects` WHERE `status`='در حال انجام'	");
+		foreach ( $rows as $row )
+		{
+			$counter+=1;
+			$message .='<tr>';
+			$message .= '<th scope="row">'. $counter .'</th>';
+			$message .= '<th>'.$row->employer.'</th>';
+			$message .= '<th>'.$row->place.'</th>';
+			$message .= '<th>'.$row->contract.'</th>';
+			$message .= '<th>'.$row->status.'</th>';
+			$message .= '<th>'.$row->volume.'</th>';
+			$message .= '<th>'.$row->date.'</th>';
+	
+			$message .='</tr>';
+		}
+		 
+		// Output needs to be return
+		return $message;
 	}elseif ($id==3){
 		$rows = $wpdb->get_results( "SELECT * FROM `wp_pk_projects`");
+		foreach ( $rows as $row )
+		{
+			$counter+=1;
+			$message .='<tr>';
+			$message .= '<th scope="row">'. $counter .'</th>';
+			$message .= '<th>'.$row->employer.'</th>';
+			$message .= '<th>'.$row->place.'</th>';
+			$message .= '<th>'.$row->contract.'</th>';
+			$message .= '<th>'.$row->status.'</th>';
+			$message .= '<th>'.$row->volume.'</th>';
+			$message .= '<th>'.$row->date.'</th>';
+	
+			$message .='</tr>';
+		}
+		 
+		// Output needs to be return
+		return $message;
 	}
-    foreach ( $rows as $row )
-	{
-		$counter+=1;
-		$message .='<tr>';
-		$message .= '<th scope="row">'. $counter .'</th>';
-		$message .= '<th>'.$row->employer.'</th>';
-		$message .= '<th>'.$row->place.'</th>';
-		$message .= '<th>'.$row->contract.'</th>';
-		$message .= '<th>'.$row->status.'</th>';
-		$message .= '<th>'.$row->volume.'</th>';
-		$message .= '<th>'.$row->date.'</th>';
 
-		$message .='</tr>';
-	}
-	 
-	// Output needs to be return
-	return $message;
 }
 
 ?>
