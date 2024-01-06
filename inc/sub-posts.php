b11eb117093d4cba815e5de83319aee3ae42333d
maestro6321 <grand.master@live.com>
2024-01-06 15:27:07 +0330

    base of post page style

--- inc/sub-posts.php
+++ inc/sub-posts.php
@@ -10,7 +10,22 @@ query_posts($query); ?>
  
 <?php if(have_posts()):
  while(have_posts()): the_post(); ?>
-          <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+          <!-- <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li> -->
+          <div class="post-content row text-right">
+        <div class="col-sm-12 col-md-7 col-12 position-relative">
+          <h5><?php the_title(); ?></h5>
+          <br>
+          <hr>
+          <a href="<?php echo get_post_permalink( get_the_id() ); ?>">
+            <button class="btn btn-warning btn-lg float-left btn-sm"> ادامه مطلب <span
+                class="spinner-grow spinner-grow-sm"></span> </button>
+          </a>
+    </div>
+    <div class=" pic col-sm-12 col-md-5 col-12 ">
+      <a href="<?php echo get_post_permalink( get_the_id() ); ?>"><img src="<?php echo get_the_post_thumbnail_url( get_the_id() );  ?>" alt=""></a>
+    </div>          
+    <br>
+    <hr>
             <!-- content -->
  
         <?php endwhile; ?>
